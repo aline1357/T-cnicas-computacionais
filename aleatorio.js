6ca5f4b90beb43990fe0ea8cf316d2768672589c
aline1357 <maria.silva.aline@escola.pr.gov.br>
2026-09-16 16:51:44 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,1 +1,13 @@
 const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const escola = "Alura Start";
+
+console.log("Eu estudo na " + escola);
+const escola = "Alura Start";
+
+console.log(["Eu estudo na ", escola].join());
+const escola = "Alura Start";
+
+console.log("Eu estudo na ".concat(escola));
+const escola = "Alura Start";
+
+console.log(`Eu estudo na ${escola}`);
