400b61e3168f4cf6cb7fda3ad1cd40dc96228b8d
aline1357 <maria.silva.aline@escola.pr.gov.br>
2026-09-16 16:38:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,5 +54,24 @@ caixaPerguntas.textContent = "Em 2049...";
 textoResultado.textContent = historiaFinal;
 caixaAlternativas.textContent = "";
 botaoJogarNovamente.addEventListener("click", jogaNovamente());
+        function mostraResultado() {
+caixaPerguntas.textContent = "Em 2049...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+caixaResultado.classList.add("mostrar"); botaoJogarNovamente.addEventListener("click", jogaNovamente());
+}
+        function jogaNovamente() {
+atual = 0;
+historiaFinal = "";
+caixaResultado.classList.remove("mostrar");
+mostraPergunta();
+}
+        function mostraResultado() {
+caixaPerguntas.textContent = "Em 2049...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+caixaResultado.classList.add("mostrar"); botaoJogarNovamente.addEventListener("click", jogaNovamente);
+}
+        
 
 }
